42dae3efcfd625ff2771cd7f62d4827c4f94767b
Emils J <ej@era.lv>
2016-09-05 17:28:33 +0300

    Implemented incremental zoom

--- dd.sh
+++ dd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 INC=0
+ZOOM="$3"
 
 if [[ -n $2 ]]; then
   INC="${2}"
@@ -17,7 +18,8 @@ while [ ! -z $1 ]; do
     python deepdream.py
     if [[ -f "/data/output/${OUTPUT}" ]]; then
       if [[ ! -z $3 ]]; then
-        python image-zoom.py "output/${OUTPUT}" "$3"
+        python image-zoom.py "output/${OUTPUT}" "$ZOOM"
+        ZOOM=$(python -c "print $ZOOM+0.005")
       fi
     else
       exit 1
